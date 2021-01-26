f016db4136f4fc80e0acf5933e09a87d5e37e4f7
IntegratedIntegral <w.boncela@gmail.com>
2021-01-26 11:20:01 +0100

    perimeter

--- MatrixRotation/main.cpp
+++ MatrixRotation/main.cpp
@@ -15,41 +15,19 @@ void print_matrix(vector<vector<int>> mt) {
 	}
 }
 
-void rotateRect(vector<vector<int>> &matrix, int index) {
-	int m = matrix.size();    // rows quantity
-	int n = matrix[0].size(); // columns quantity
+void rotateRect(vector<vector<int>> &matrix, int index, int r) {
+	int rect_height = matrix.size() - 2 * index;
+	int rect_width = matrix[0].size() - 2 * index;
+	int perimeter = 2 * (rect_height + rect_width) - 4;
 
-	int first = matrix[index][index];
-
-	//upper side
-	for (int j = index; j < n - index - 1; j++) {
-		matrix[index][j] = matrix[index][j + 1];
-	}
-
-	//right side
-	for (int i = index; i < m - index - 1; i++) {
-		matrix[i][n - index - 1] = matrix[i + 1][n - index - 1];
-	}
-
-	//lower side
-	for (int j = n - index - 1; j > index; j--) {
-		matrix[m - index - 1][j] = matrix[m - index - 1][j - 1];
-	}
-
-	//left side
-	for (int i = m - index - 1; i > index; i--) {
-		matrix[i][index] = matrix[i - 1][index];
-	}
-
-	matrix[index + 1][index] = first;
+	
 }
 
 void matrixRotation(vector<vector<int>>& matrix, int r) {
 	int min_dim = min(matrix.size(), matrix[0].size());
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < min_dim / 2; j++) {
-			rotateRect(matrix, j);
-		}
+	// rotate each layer by r
+	for (int j = 0; j < min_dim / 2; j++) {
+		rotateRect(matrix, j, r);
 	}
 	int m = matrix.size();
 	int n = matrix[0].size();
